3c3f9a56ef337ba7e1c08e20e35b838195e76d68
KMINNAM <54440089+KMINNAM@users.noreply.github.com>
2019-09-03 16:01:01 +0200

    Add files via upload

---
+++ Graaf_Fig5p6_ver5p0.m
@@ -0,0 +1,166 @@
+% Graaf_Fig5_6_ver5.0.m
+% Written by Kyungmin Nam
+% Email: K.M.Nam@umcutrecht.nl
+% Started: 27/08/2019, Last modified: 03/09/2019
+%--------------------------------------------------------------------------
+clear variables; close all; clc;
+
+data_dir      = 'F:\mfiles_kmin\github\sLASER_FOCI';
+output_dir    = 'F:\mfiles_kmin\github\sLASER_FOCI\output'; mkdir(output_dir);
+data_filename = 'Graaf_Fig5p6_ver5p0';
+
+% Robin_de_Graaf figure 5.6
+dt  = 6.4e-6; % RF dwell time [sec]
+tau = 1e-3;   % pulse duration, T [sec]
+n   = 3;      % zero crossing number
+
+% Define discrete samples of time over [0 T)
+nt = ceil(tau/dt);    % number of samples
+t  = (0:nt-1).' * dt; % [sec]
+
+alpha_excitation = 90;  % exciation flip angle [degrees]
+alpha_inversion  = 180; % inversion flip angle [degrees]
+
+fb = sinc(2*n/tau*(t-(tau/2)));
+fb = fb / max(fb);
+
+gamma = 4257.784679; % Gamma for current nucleus [Hz/G]
+b1max_excitation = (alpha_excitation / 360) / (gamma * sum(fb) * dt) * 1e2; % [uT]
+fprintf(sprintf('b1max_excitation = %f\n', b1max_excitation));
+
+b1max_inversion = (alpha_inversion / 360) / (gamma * sum(fb) * dt) * 1e2; % [uT]
+fprintf(sprintf('b1max_inversion = %f\n', b1max_inversion));
+
+%% Perform Bloch simulation
+df       = 1;   % sampling interval in frequency domain [Hz]
+nf       = 2e4; % number of samples in frequency domain
+Delta_Hz = (-floor(nf/2):ceil(nf/2)-1).' * df; % [Hz]
+mx0 = zeros(nf,1); my0 = zeros(nf,1); mz0 = ones(nf,1);
+
+b1_sinc_excitation = b1max_excitation * fb; % [uT]
+b1_sinc_inversion = b1max_inversion * fb; % [uT]
+
+% Calculate the excitation profile at on-resonance on SINC
+% [mx,my,mz] = bloch(b1, gr, tp, t1, t2, df, dp, mode, mx, my, mz)
+[mx_sinc_excitation,my_sinc_excitation,mz_sinc_excitation] = ...
+    bloch(b1_sinc_excitation*1e-2, zeros(nt,1), dt, 1e2, 1e2, Delta_Hz, 0, 0, mx0, my0, mz0);
+
+scale_factor = max(abs(mx_sinc_excitation));
+mx_sinc_excitation = mx_sinc_excitation / scale_factor;
+
+scale_factor = max(abs(my_sinc_excitation));
+my_sinc_excitation = my_sinc_excitation / scale_factor;
+
+mxy_sinc_excitation = mx_sinc_excitation + 1j*my_sinc_excitation;
+
+[mx_sinc_inversion,my_sinc_inversion_,mz_sinc_inversion] = ...
+    bloch(b1_sinc_inversion*1e-2, zeros(nt,1), dt, 1e2, 1e2, Delta_Hz, 0, 0, mx0, my0, mz0);
+
+%% Display figures 5.6 (A, B, C)
+LineWidth = 1.5;
+black = [0 0 0];
+gray  = [0.5 0.5 0.5];
+
+figure('color', 'w','units','normalized','outerposition',[0 0 1 1]); 
+% Display 5.6(A)
+subplot(2,3,1), plot(t*1e3, b1_sinc_excitation, 'k', 'LineWidth', LineWidth); hold on;
+plot(t*1e3, zeros(length(t),1), 'k--'); hold off; set(gca, 'Box', 'off');
+xlabel('Time (ms)'); ylabel('RF amplitude (uT)');
+
+% Display 5.6(B)
+subplot(2,3,2), hold on;
+plot(Delta_Hz*1e-3, mx_sinc_excitation, 'r', 'LineWidth', LineWidth);
+plot(Delta_Hz*1e-3, my_sinc_excitation, 'b', 'LineWidth', LineWidth);
+plot(Delta_Hz*1e-3, abs(mxy_sinc_excitation), 'k', 'LineWidth', LineWidth);
+legend('M_x','M_y','M_{xy}','Location','Southeast'); hold off;
+axis([-1e1 1e1 -1.1 1.1]); xlabel('Frequency (kHz)'); ylabel('M_{xy}/M_0');
+
+% Display 5.6(C)
+subplot(2,3,3), plot(Delta_Hz*1e-3, mz_sinc_inversion, 'k', 'LineWidth', LineWidth); hold on;
+plot(Delta_Hz*1e-3, zeros(length(Delta_Hz),1), 'k--'); hold off; set(gca, 'Box', 'off');
+xlabel('Frequency (kHz)'); ylabel('M_z/M_0');
+
+%%
+% Robin_de_Graaf figure 5.6 (D)
+
+beta1 = -log(0.01);
+beta2 = -log(0.1);
+
+gaussian_func = exp(-(2/tau*(t-tau/2)).^2);
+
+b1max_excitation = (alpha_excitation / 360) / (gamma * sum(gaussian_func) * dt) * 1e2; % [uT]
+
+b1_gaussian_excitation_beta1 = b1max_excitation * exp(-beta1.*(2/tau*(t-tau/2)).^2);
+b1_gaussian_excitation_beta2 = b1max_excitation * exp(-beta2.*(2/tau*(t-tau/2)).^2);
+
+gaussian_func_beta1 = exp(-beta1.*(2/tau*(t-tau/2)).^2);
+gaussian_func_beta2 = exp(-beta2.*(2/tau*(t-tau/2)).^2);
+
+b1max_inversion_beta1 = (alpha_inversion / 360) / (gamma * sum(gaussian_func_beta1) * dt) * 1e2; % [uT]
+b1max_inversion_beta2 = (alpha_inversion / 360) / (gamma * sum(gaussian_func_beta2) * dt) * 1e2; % [uT]
+
+b1_gaussian_inversion_beta1 = b1max_inversion_beta1 * exp(-beta1.*(2/tau*(t-tau/2)).^2);
+b1_gaussian_inversion_beta2 = b1max_inversion_beta2 * exp(-beta2.*(2/tau*(t-tau/2)).^2);
+
+% Calculate the excitation profile at on-resonance on Gaussian
+% [mx,my,mz] = bloch(b1, gr, tp, t1, t2, df, dp, mode, mx, my, mz)
+% Excitation 
+[mx_gaussian_excitation_beta1,my_gaussian_excitation_beta1,mz_gaussian_excitation_beta1] = ...
+    bloch(b1_gaussian_excitation_beta1*1e-2, zeros(nt,1), dt, 1e10, 1e10, Delta_Hz, 0, 0, mx0, my0, mz0);
+
+scale_factor = max(mx_gaussian_excitation_beta1);
+mx_gaussian_excitation_beta1 = mx_gaussian_excitation_beta1/scale_factor;
+
+scale_factor = max(my_gaussian_excitation_beta1);
+my_gaussian_excitation_beta1 = my_gaussian_excitation_beta1/scale_factor;
+
+mxy_gaussian_excitation_beta1 = mx_gaussian_excitation_beta1 + 1j*my_gaussian_excitation_beta1;
+
+% [mx_gaussian_excitation_beta2,my_gaussian_excitation_beta2,mz_gaussian_excitation_beta2] = ...
+%     bloch(b1_gaussian_excitation_beta2*1e-2, zeros(nt,1), dt, 1e2, 1e2, Delta_Hz, 0, 0, mx0, my0, mz0);
+
+% scale_factor = max(abs(mx_gaussian_excitation_beta1));
+% mx_gaussian_excitation_beta = mx_gaussian_excitation_beta1 / scale_factor;
+% 
+% scale_factor = max(abs(my_gaussian_excitation_beta1));
+% my_gaussian_excitation_beta1 = my_gaussian_excitation_beta1 / scale_factor;
+
+% mxy_gaussian_excitation_beta1 = mx_gaussian_excitation_beta1 + 1j*my_gaussian_excitation_beta1;
+
+% Inversion
+[mx_gaussian_inversion_beta1,my_gaussian_inversion_beta1,mz_gaussian_inversion_beta1] = ...
+    bloch(b1_gaussian_inversion_beta1*1e-2, zeros(nt,1), dt, 1e2, 1e2, Delta_Hz, 0, 0, mx0, my0, mz0);
+mxy_gaussian_inversion_beta1 = mx_gaussian_inversion_beta1 + 1j*my_gaussian_inversion_beta1;
+
+[mx_gaussian_inversion_beta2,my_gaussian_inversion_beta2,mz_gaussian_inversion_beta2] = ...
+    bloch(b1_gaussian_inversion_beta2*1e-2, zeros(nt,1), dt, 1e2, 1e2, Delta_Hz, 0, 0, mx0, my0, mz0);
+
+%--------------------------------------------------------------------------
+% Display 5.6 (D, E, F)
+%--------------------------------------------------------------------------
+% Display 5.6 (D)
+subplot(2,3,4), plot(t*1e3, b1_gaussian_excitation_beta1, 'Color', gray, 'LineWidth', LineWidth);
+hold on; plot(t*1e3, b1_gaussian_excitation_beta2, 'Color', black, 'LineWidth', LineWidth); hold off;
+set(gca, 'Box', 'off'); xlabel('Time (ms)'); ylabel('RF amplitude');
+
+% Display 5.6 (E)
+subplot(2,3,5), hold on;
+plot(Delta_Hz*1e-3, mx_gaussian_excitation_beta1, 'r', 'LineWidth', LineWidth);
+plot(Delta_Hz*1e-3, my_gaussian_excitation_beta1, 'b', 'LineWidth', LineWidth);
+plot(Delta_Hz*1e-3, abs(mxy_gaussian_excitation_beta1), 'k', 'LineWidth', LineWidth);
+legend('M_x','M_y','M_{xy}','Location','Southeast'); hold off;
+axis([-1e1 1e1 -1.1 1.1]); xlabel('Frequency (kHz)'); ylabel('M_{xy}/M_0');
+
+% Display 5.6 (F)
+subplot(2,3,6), plot(Delta_Hz*1e-3, mz_gaussian_inversion_beta1, 'Color', gray, 'LineWidth', LineWidth); hold on;
+plot(Delta_Hz*1e-3, mz_gaussian_inversion_beta2, 'Color', black, 'LineWidth', LineWidth); 
+plot(Delta_Hz*1e-3, zeros(length(Delta_Hz*1e-3),1), 'k--'); hold off; 
+set(gca, 'Box', 'off'); xlabel('Frequency (kHz)'); ylabel('M_z/M_0');
+legend('1% gauss','10% gauss','Location','Southeast');
+
+export_fig(fullfile(output_dir, data_filename), '-m2', '-tif'); close;
+
+
+
+
+
